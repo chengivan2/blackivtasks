7b4094ccfccd89b91126256402ef9c0a65b693fc
chengivan <chengoleivan@gmail.com>
2024-04-14 18:25:36 +0300

    Initial commit. KindeAuth: sign up, login and log out working

---
+++ src/app/components/signedin.jsx
@@ -0,0 +1,22 @@
+import { LoginLink, RegisterLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export default async function SignedIn() {
+  const { isAuthenticated } = getKindeServerSession();
+
+  return (await isAuthenticated()) ? (
+    <div>
+      <p>Yay! Signed in.</p>
+      <LogoutLink>Log out</LogoutLink>
+    </div>
+  ) : (
+    <div>
+      <p>
+      This page is protected, please <LoginLink><b>Login</b></LoginLink> to view it.
+      </p>
+      <p>
+      If you don't have an account, you can <RegisterLink><b>Sign Up</b></RegisterLink>.
+      </p>
+    </div>
+  );
+}
