78cecb0184976a83b2b119b13fd6ff28f10d5838
chengivan <chengoleivan@gmail.com>
2024-04-17 00:21:58 +0300

    Fixed the handler being called from the addnewtask api route

--- src/app/api/add_new_task/route.js
+++ src/app/api/add_new_task/route.js
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export default async function handler(req, res) {
+export default async function POST(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method Not Allowed" });
   }
